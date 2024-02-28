97d64773ec6268b4b45f56b1d8d71bd4b1907b99
tintin010 <82323029+tintin010@users.noreply.github.com>
2024-02-29 01:14:14 +0900

    Update dbinfo.py

--- init/dbinfo.py
+++ init/dbinfo.py
@@ -3,6 +3,6 @@
 
 db_host = "database-1.cv4i5qfeypku.ap-northeast-2.rds.amazonaws.com"    #엔드포인트
 db_username = "admin"        # 아이디
-db_password = "ad_____"     # 비번
+db_password = "ad_____"     # 비번은 따로 설정함
 db_name = "BCDD"
 db_port = 3306
